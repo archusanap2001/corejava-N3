e07e7ac575c4a551c196fde72b292a463d55f406
ASUS <ASUS@DESKTOP-KU180G7>
2023-02-01 23:01:02 +0530

    first commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java full stack training");
+		// TODO Auto-generated method stub
+
+	}
+
+}
